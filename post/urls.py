40bb30b733dfc6543e5d407f502f4c1ae0ba687e
emansamsalem <emansamirsamir543@gmail.com>
2021-05-09 13:08:18 +0200

    using class based view

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.post_list, name='post_list1'),
     path('<int:id>',views.post_detail, name='post_detail1'),
     path('create', views.post_create, name='post_create1'),
-    path('<int:id>/edit', views.post_edit, name='post_edit1')
+    path('<int:id>/edit', views.post_edit, name='post_edit1'),
+    path('cbv', views.PostList.as_view(), name = 'cbv_post_list'),
+    path('cbv/<int:pk>', views.PostDetail.as_view(), name='cbv_post_detail'),
+    path('cbv/<int:pk>/edit', views.PostUpdate.as_view(), name='cbv_post_update'),
+    path('cbv/create', views.PostCreate.as_view(), name='cbv_post_create')
 
 ]
--- post/views.py
+++ post/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 #added
 from .models import Post
 from . forms import post_form
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
 
 def post_list(request):
     all_posts = Post.objects.all() #RETURNS all created objects
-    return render(request,'post/all_posts.html',{'all_posts':all_posts})
+    return render(request,'post/post_list.html',{'all_posts':all_posts})
 
 
 
@@ -37,3 +40,24 @@ def post_edit(request, id):
         form = post_form(instance=post)
     return render(request, 'post/post_edit.html', {'form': form})
 
+
+class PostList(ListView):
+    model = Post
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post/post_detail.html'
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'created_at']  # as it doesn't access forms.py file
+    template_name = 'post/post_edit.html'  # as the need file is named post_edit and edit differs from update
+    success_url = '/blog/cbv'   #redirected link when submit button is pressed
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'content', 'created_at','published', 'email','views_count', 'Category' ]  # as it doesn't access forms.py file
+    template_name = 'post/post_create.html'
+    success_url = '/blog/cbv'  # redirected link when submit button is pressed
